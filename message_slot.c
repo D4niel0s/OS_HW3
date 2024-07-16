e6a2ea97b7a5732043136b910b547a3ac3952e12
D4niel0s <das.volkov@gmail.com>
2024-07-16 23:06:57 +0300

    Added files from recitation when deleting all the trash

---
+++ message_slot.c
@@ -0,0 +1,113 @@
+// Declare what kind of code we want
+// from the header files. Defining __KERNEL__
+// and MODULE allows us to access kernel-level
+// code not usually available to userspace programs.
+#undef __KERNEL__
+#define __KERNEL__
+#undef MODULE
+#define MODULE
+
+
+#include <linux/kernel.h>   /* We're doing kernel work */
+#include <linux/module.h>   /* Specifically, a module */
+#include <linux/fs.h>       /* for register_chrdev */
+#include <linux/uaccess.h>  /* for get_user and put_user */
+#include <linux/string.h>   /* for memset. NOTE - not string.h!*/
+
+MODULE_LICENSE("GPL");
+
+//Our custom definitions of IOCTL operations
+#include "message_slot.h"
+
+// The message the device will give when asked
+static char the_message[BUF_LEN];
+
+
+//================== DEVICE FUNCTIONS ===========================
+static int device_open( struct inode* inode,
+                        struct file*  file )
+{
+    return SUCCESS;
+}
+
+//---------------------------------------------------------------
+static int device_release( struct inode* inode,
+                           struct file*  file)
+{
+  return SUCCESS;
+}
+
+//---------------------------------------------------------------
+// a process which has already opened
+// the device file attempts to read from it
+static ssize_t device_read( struct file* file,
+                            char __user* buffer,
+                            size_t       length,
+                            loff_t*      offset )
+{
+  return -EINVAL;
+}
+
+//---------------------------------------------------------------
+// a processs which has already opened
+// the device file attempts to write to it
+static ssize_t device_write( struct file*       file,
+                             const char __user* buffer,
+                             size_t             length,
+                             loff_t*            offset)
+{
+  return 1;
+}
+
+//----------------------------------------------------------------
+static long device_ioctl( struct   file* file,
+                          unsigned int   ioctl_command_id,
+                          unsigned long  ioctl_param )
+{
+  return SUCCESS;
+}
+
+//==================== DEVICE SETUP =============================
+
+// This structure will hold the functions to be called
+// when a process does something to the device we created
+struct file_operations Fops = {
+  .owner	  = THIS_MODULE, 
+  .read           = device_read,
+  .write          = device_write,
+  .open           = device_open,
+  .unlocked_ioctl = device_ioctl,
+  .release        = device_release,
+};
+
+//---------------------------------------------------------------
+// Initialize the module - Register the character device
+static int __init simple_init(void)
+{
+  int rc = -1;
+
+  // Register driver capabilities. Obtain major num
+  rc = register_chrdev( MAJOR_NUM, DEVICE_RANGE_NAME, &Fops );
+
+  // Negative values signify an error
+  if( rc < 0 ) {
+    printk( KERN_ALERT "%s registraion failed for  %d\n",
+                       DEVICE_FILE_NAME, MAJOR_NUM );
+    return rc;
+  }
+  return 0;
+}
+
+//---------------------------------------------------------------
+static void __exit simple_cleanup(void)
+{
+  // Unregister the device
+  // Should always succeed
+  unregister_chrdev(MAJOR_NUM, DEVICE_RANGE_NAME);
+}
+
+//---------------------------------------------------------------
+module_init(simple_init);
+module_exit(simple_cleanup);
+
+//========================= END OF FILE =========================
