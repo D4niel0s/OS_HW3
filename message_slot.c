20d494871ef8d2eae970ec992f480e8d4613cafd
D4niel0s <das.volkov@gmail.com>
2024-07-17 01:09:08 +0300

    ioctl() Done (still needs check)

--- message_slot.c
+++ message_slot.c
@@ -7,32 +7,31 @@
 #undef MODULE
 #define MODULE
 
-
 #include <linux/kernel.h>   /* We're doing kernel work */
 #include <linux/module.h>   /* Specifically, a module */
 #include <linux/fs.h>       /* for register_chrdev */
 #include <linux/uaccess.h>  /* for get_user and put_user */
 #include <linux/string.h>   /* for memset. NOTE - not string.h!*/
+#include <linux/slab.h>   /* for kmalloc() */
 
 MODULE_LICENSE("GPL");
 
-//Our custom definitions of IOCTL operations
 #include "message_slot.h"
 
-// The message the device will give when asked
-static char the_message[BUF_LEN];
+/* An array that will hold every message slot's channels linked list*/
+channels all_slots[256];
+
+
 
 
 //================== DEVICE FUNCTIONS ===========================
-static int device_open( struct inode* inode,
-                        struct file*  file )
+static int device_open( struct inode* inode, struct file* file)
 {
     return SUCCESS;
 }
 
 //---------------------------------------------------------------
-static int device_release( struct inode* inode,
-                           struct file*  file)
+static int device_release( struct inode* inode, struct file* file)
 {
   return SUCCESS;
 }
@@ -56,6 +55,7 @@ static ssize_t device_write( struct file*       file,
                              size_t             length,
                              loff_t*            offset)
 {
+
   return 1;
 }
 
@@ -64,8 +64,51 @@ static long device_ioctl( struct   file* file,
                           unsigned int   ioctl_command_id,
                           unsigned long  ioctl_param )
 {
+  unsigned int minor, i;
+  channel *iterator, *last, *tmp;
+
+  if(ioctl_command_id != MSG_SLOT_CHANNEL || ioctl_param == 0){
+    return -EINVAL;
+  }
+
+  minor = iminor(file_inode(file));
+
+  iterator = all_slots[minor].head;
+  i=0;
+  while(iterator != NULL){
+    i++;
+
+    if(iterator->channel_id == ioctl_param){
+      break;
+    }
+    last = iterator;
+    iterator = iterator->next;
+  }
+
+  if(iterator == NULL){ /*Channel does not exist - either because empty list or because it wasn't created yet*/
+    tmp = (channel *)kmalloc(sizeof(channel), GFP_KERNEL);
+
+    if(!tmp){
+      printk("Failed to allocate memory");
+      return -1;
+    }
+
+    tmp->channel_id = ioctl_param;
+    tmp->msg = "";
+    tmp->next = NULL;
+
+    if(i == 0){ /*Empty channels list*/
+      all_slots[minor].head = tmp;
+    }else{
+      last->next = tmp;
+    }
+    iterator = tmp;
+  }
+
+  file->private_data = (void *)iterator;
+
   return SUCCESS;
-}
+  }
 
 //==================== DEVICE SETUP =============================
 
@@ -80,25 +123,25 @@ struct file_operations Fops = {
   .release        = device_release,
 };
 
-//---------------------------------------------------------------
-// Initialize the module - Register the character device
+
 static int __init simple_init(void)
 {
-  int rc = -1;
+  int rc = -1 ,i;
 
-  // Register driver capabilities. Obtain major num
-  rc = register_chrdev( MAJOR_NUM, DEVICE_RANGE_NAME, &Fops );
+  for(i=0;i<256;++i){
+    all_slots[i].head = NULL; /* Init all lists to be empty */
+  }
+  
+  rc = register_chrdev(MAJOR_NUM, DEVICE_RANGE_NAME, &Fops);
 
-  // Negative values signify an error
-  if( rc < 0 ) {
-    printk( KERN_ALERT "%s registraion failed for  %d\n",
-                       DEVICE_FILE_NAME, MAJOR_NUM );
+  if(rc < 0) {
+    printk( KERN_ALERT "%s registraion failed for  %d\n", DEVICE_FILE_NAME, MAJOR_NUM );
     return rc;
   }
-  return 0;
+
+  return SUCCESS;
 }
 
-//---------------------------------------------------------------
 static void __exit simple_cleanup(void)
 {
   // Unregister the device
