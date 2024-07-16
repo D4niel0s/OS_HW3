0b55d0e8ca2d671a2240fe1cccfb16a5549b62c2
D4niel0s <das.volkov@gmail.com>
2024-07-17 01:08:35 +0300

    Changed header to fit this HW and added data structures

--- message_slot.h
+++ message_slot.h
@@ -1,21 +1,28 @@
-#ifndef CHARDEV_H
-#define CHARDEV_H
+#ifndef MESSAGE_SLOT_H
+#define MESSAGE_SLOT_H
 
 #include <linux/ioctl.h>
 
-// The major device number.
-// We don't rely on dynamic registration
-// any more. We want ioctls to know this
-// number at compile time.
-//#define MAJOR_NUM 244
-#define MAJOR_NUM 236
 
-// Set the message of the device driver
-#define IOCTL_SET_ENC _IOW(MAJOR_NUM, 0, unsigned long)
-
+#define MAJOR_NUM 235
+#define MSG_SLOT_CHANNEL _IOW(MAJOR_NUM, 0, unsigned long)
 #define DEVICE_RANGE_NAME "message_slot"
-#define BUF_LEN 80
+#define BUF_LEN 128
 #define DEVICE_FILE_NAME "message_slot"
 #define SUCCESS 0
+
+/* A single channel in the message slot*/
+typedef struct chan{
+    unsigned int channel_id;
+    char msg[BUF_LEN];
+    struct chan *next;
+} channel;
+
+/* A linked list which will contain all channels of current message slot*/
+typedef struct chans{
+    channel *head;
+} channels;
+
+
 
 #endif
