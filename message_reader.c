3457b8d3d5382780f698fdf2ce66e0b103fcf2cf
D4niel0s <das.volkov@gmail.com>
2024-07-17 21:17:50 +0300

    Added reader and sender (finished)

--- message_reader.c
+++ message_reader.c
@@ -8,20 +8,22 @@
 #include "message_slot.h"
 
 
-
 int main(int argc, char **argv){
-    int fd, len, ret, id;
 
-    if(argc != 4){
+    int fd, ret, len, id;
+    char buf[BUF_LEN];
+
+    if(argc != 3){
         perror("Error: invalid amount of arguments\n");
         exit(1);
     }
 
-    fd = open(argv[1], O_WRONLY);
+    fd = open(argv[1], O_RDONLY);
     if(fd < 0){
         perror("Error: could not open file\n");
         exit(1);
     }
+
     id = atoi(argv[2]);
     ret = ioctl(fd, MSG_SLOT_CHANNEL, id);
     if(ret < 0){
@@ -29,9 +31,14 @@ int main(int argc, char **argv){
         exit(1);
     }
 
-    len = strlen(argv[3]);
-    len = write(fd, argv[3], len);
+    len = read(fd, buf, BUF_LEN);
     if(len < 0){
+        perror("Error: read failed\n");
+        exit(1);
+    }
+
+    ret = write(1, buf, len);
+    if(ret < 0){
         perror("Error: write failed\n");
         exit(1);
     }
